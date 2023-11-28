dcce0089cf31865d2de4c5b8e0cb7df412ae96d2
HrushikeshKoppula <95178027+HrushikeshKoppula@users.noreply.github.com>
2023-11-28 15:52:24 +0530

    finally was able to solve drawing a predefined var

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 running = True
 
+ball = pygame.Rect(0, 0, 20, 20)
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill("violet")
+    pygame.draw.rect(screen, "red", ball)
     pygame.display.flip()
     clock.tick(60)
 
